a740b9fae4026d61b488116da842f7c78dff9b8b
Sant1ag0x00 <Hofmann_tim@protonmail.ch>
2022-12-01 11:08:49 +0100

    update solution 01 to accomodate part 2

--- 01/main.py
+++ 01/main.py
@@ -1,6 +1,8 @@
-def main():
+#function that takes the number of elves from the top as a parameter and returns the sum of their calories
+def get_caloriecount(topx: int):
     with open("input.txt") as f:
         lines = f.readlines()
+    calorie_sum = 0
     temp_list = []
     final_list = []
     for entry in lines:
@@ -12,6 +14,10 @@ def main():
             temp_list.append(int(entry))
     for elf in final_list:
         final_list[final_list.index(elf)] = sum(elf)
-    print(final_list[final_list.index(max(final_list))])
+    #print(final_list[final_list.index(max(final_list))])
+    final_list.sort()
+    for i in range(topx):
+        calorie_sum += final_list.pop()
+    print(calorie_sum)
 
-main()
+get_caloriecount(3)
